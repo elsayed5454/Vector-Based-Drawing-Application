535d6a3f69995c0d474697ff5171294cf17dc1ce
Seif45 <48135395+Seif45@users.noreply.github.com>
2019-10-27 16:29:14 +0200

    GeometricShapes implem

--- src/eg/edu/alexu/csd/oop/draw/GeometricShapes.java
+++ src/eg/edu/alexu/csd/oop/draw/GeometricShapes.java
@@ -3,67 +3,64 @@ package eg.edu.alexu.csd.oop.draw;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.HashMap;
 import java.util.Map;
 
 public class GeometricShapes implements Shape {
 
+	private Point point = new Point();
+	private Map<String, Double> prop = new HashMap<String, Double>() ;
+	private Color clr ;
+	private Color fillClr ;
+	private Graphics draw ;
+	
 	@Override
 	public void setPosition(Point position) {
-		// TODO Auto-generated method stub
-		
+		this.point = position ;
 	}
 
 	@Override
 	public Point getPosition() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.point;
 	}
 
 	@Override
 	public void setProperties(Map<String, Double> properties) {
-		// TODO Auto-generated method stub
-		
+		this.prop = properties;
 	}
 
 	@Override
 	public Map<String, Double> getProperties() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.prop;
 	}
 
 	@Override
 	public void setColor(Color color) {
-		// TODO Auto-generated method stub
-		
+		this.clr = color ;
 	}
 
 	@Override
 	public Color getColor() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.clr;
 	}
 
 	@Override
 	public void setFillColor(Color color) {
-		// TODO Auto-generated method stub
-		
+		this.fillClr = color;
 	}
 
 	@Override
 	public Color getFillColor() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.fillClr;
 	}
 
 	@Override
 	public void draw(Graphics canvas) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public Object clone() throws CloneNotSupportedException{
-		return null;
+		return draw.create();
 	}
 
 }
