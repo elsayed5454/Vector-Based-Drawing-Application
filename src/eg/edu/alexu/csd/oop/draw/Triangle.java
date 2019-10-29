17aff214ba1855aa21fe7874968338d6191ed7a5
Elsayed Akram <elsayed5454@gmail.com>
2019-10-29 19:26:25 +0200

    GUI class

--- src/eg/edu/alexu/csd/oop/draw/Triangle.java
+++ src/eg/edu/alexu/csd/oop/draw/Triangle.java
@@ -1,44 +1,12 @@
 package eg.edu.alexu.csd.oop.draw;
 
-<<<<<<< HEAD
-import java.awt.BasicStroke;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-
-||||||| merged common ancestors
-=======
 import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
->>>>>>> 78988d26ab5fb3a0e1bec269420983ff97717d53
+
 public class Triangle extends Polygons {
 	
-	private int[] x;
-	private int[] y;
-
-<<<<<<< HEAD
-	public Triangle(Point p1, Point p2, Point p3) {
-		
-		x[0] = p1.x;
-		x[1] = p2.x;
-		x[2] = p3.x;
-		y[0] = p1.y;
-		y[1] = p2.y;
-		y[2] = p3.y;
-	}
-	
-	@Override
-	public void draw(Graphics canvas) {
-		((Graphics2D)canvas).setStroke(new BasicStroke(2.0f));
-		((Graphics2D)canvas).drawPolygon(x, y, 3);
-		((Graphics2D)canvas).setColor(getColor());
-		((Graphics2D)canvas).setColor(getFillColor());
-		((Graphics2D)canvas).fillPolygon(x, y, 3);
-	}
-||||||| merged common ancestors
-=======
 	public Triangle () {
 	this.prop.put("x1", getPosition().getX());
 	this.prop.put("y1", getPosition().getY());
@@ -59,5 +27,4 @@ public class Triangle extends Polygons {
 		((Graphics2D)canvas).setColor(getFillColor());
 		((Graphics2D)canvas).drawPolygon( x , y , 3);
 	}
->>>>>>> 78988d26ab5fb3a0e1bec269420983ff97717d53
 }
