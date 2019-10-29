78988d26ab5fb3a0e1bec269420983ff97717d53
Seif45 <48135395+Seif45@users.noreply.github.com>
2019-10-29 16:54:23 +0200

    triangle and logic main methods

---
+++ src/eg/edu/alexu/csd/oop/draw/Logic.java
@@ -0,0 +1,66 @@
+package eg.edu.alexu.csd.oop.draw;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Logic implements DrawingEngine {
+
+	private List<Shape> shapes = new ArrayList<Shape>();
+	
+	@Override
+	public void refresh(Graphics canvas) {
+		Shape[] drawnShapes = getShapes() ;
+		for (int i = 0 ; i < drawnShapes.length ; i++) {
+			drawnShapes[i].draw(canvas);
+		}
+	}
+
+	@Override
+	public void addShape(Shape shape) {
+		shapes.add(shape);
+	}
+
+	@Override
+	public void removeShape(Shape shape) {
+		shapes.remove(shape);
+	}
+
+	@Override
+	public void updateShape(Shape oldShape, Shape newShape) {
+		shapes.set(shapes.indexOf(oldShape), newShape);
+	}
+
+	@Override
+	public Shape[] getShapes() {
+		Shape[] drawnShapes = new Shape[shapes.size()];
+		return shapes.toArray(drawnShapes);
+	}
+
+	@Override
+	public List<Class<? extends Shape>> getSupportedShapes() {
+		return null;
+	}
+
+	@Override
+	public void undo() {
+
+	}
+
+	@Override
+	public void redo() {
+
+	}
+
+	@Override
+	public void save(String path) {
+
+	}
+
+	@Override
+	public void load(String path) {
+
+	}
+
+}
--- src/eg/edu/alexu/csd/oop/draw/Triangle.java
+++ src/eg/edu/alexu/csd/oop/draw/Triangle.java
@@ -1,5 +1,29 @@
 package eg.edu.alexu.csd.oop.draw;
 
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public class Triangle extends Polygons {
 
+	public Triangle () {
+	this.prop.put("x1", getPosition().getX());
+	this.prop.put("y1", getPosition().getY());
+	this.prop.put("x2", getPosition().getX());
+	this.prop.put("y2", getPosition().getY());
+	this.prop.put("x3", getPosition().getX());
+	this.prop.put("y3", getPosition().getY());
+	setProperties(prop);
+	}
+	
+	@Override
+	public void draw(Graphics canvas) {
+		((Graphics2D)canvas).setStroke(new BasicStroke(2.0f));
+		int[] x = { getProperties().get("x1").intValue(), getProperties().get("x2").intValue(), getProperties().get("x3").intValue() } ;
+		int[] y = { getProperties().get("y1").intValue(), getProperties().get("y2").intValue(), getProperties().get("y3").intValue() } ;
+		((Graphics2D)canvas).fillPolygon( x , y , 3);
+		((Graphics2D)canvas).setColor(getColor());
+		((Graphics2D)canvas).setColor(getFillColor());
+		((Graphics2D)canvas).drawPolygon( x , y , 3);
+	}
 }
